1e68224b5ac83ba690db62406cd83a330f5daa42
Mirikaa <163854631+Mirikaa@users.noreply.github.com>
2024-06-05 23:08:56 +0800

    Initial Commit

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAo09mrYsnzvAAkIpGOaWpWtIPd-qJyOVE",
+    authDomain: "crud-main-a25d0.firebaseapp.com",
+    databaseURL: "https://crud-main-a25d0-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "crud-main-a25d0",
+    storageBucket: "crud-main-a25d0.appspot.com",
+    messagingSenderId: "899718678374",
+    appId: "1:899718678374:web:d4c526dac36b201ac02aa7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
